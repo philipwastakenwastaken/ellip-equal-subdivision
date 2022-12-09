40561a3ebd9ee868234f2dede867b680b1b8ba4d
Philip Tofte Nielsen <s174274@student.dtu.dk>
2022-12-09 22:40:28 +0100

    Comments and command line arguments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import math
 from ellipse import Ellipse
 import matplotlib.pyplot as plt
-import numpy as np
 from scipy.optimize import minimize
+import sys
 
 
 def arc_length_loss_function(theta, ellipse, arc_subdiv, r):
@@ -11,34 +11,38 @@ def arc_length_loss_function(theta, ellipse, arc_subdiv, r):
     # MSE estimator
     return (dist - arc_subdiv) ** 2
 
-def points_to_file(X, Y):
-    with open('data.txt', 'w') as f:
+def points_to_file(path, X, Y):
+    with open(path, 'w') as f:
         f.truncate(0)
         
         for x, y in zip(X, Y):
             f.writelines(f'{x:.5f} {y:.5f}\n')
 
 if __name__ == '__main__':
-    total_rad = 2 * math.pi
-    a = 4
-    b = 2
+    # Ellipse dimensions
+    a = float(sys.argv[1])
+    b = float(sys.argv[2])
 
+    # Number of holes
     n_holes = 30
+    total_rad = 2 * math.pi
     radian_per_hole = total_rad / n_holes
 
     ellipse = Ellipse(a, b)
     arc_length, _ = ellipse.arc_length()
     arc_subdiv = arc_length / n_holes
 
+    # Knowing the size of each arc length, we must now segment the ellipse.
     t = 0
     x_coords = []
     y_coords = []
     ts = []
     for i in range(n_holes):
-        # estimate of next t-value
-        next_t = t + radian_per_hole 
+        # Estimate of next t-value
+        t_prime = t + radian_per_hole 
 
-        optim = minimize(arc_length_loss_function, next_t, args=(ellipse, arc_subdiv, t)) 
+        # Find a segment (t, t_prime) whose arc length is equal to arc_subdiv by optimization.
+        optim = minimize(arc_length_loss_function, t_prime, args=(ellipse, arc_subdiv, t)) 
         t = optim.x[0]
         ts.append(t)
         
@@ -46,17 +50,8 @@ if __name__ == '__main__':
         x_coords.append(x)
         y_coords.append(y)
         
-    points_to_file(x_coords, y_coords)
+    points_to_file('data.txt', x_coords, y_coords)
 
-    old_method_x = []
-    old_method_y = []
-    a = 0
-    for i in range(n_holes):
-        x, y = ellipse.point(a)
-        old_method_x.append(x)
-        old_method_y.append(y)
-        a += radian_per_hole
-    
     plt.figure(figsize=(8, 5))
     plt.plot(x_coords, y_coords, 'o')
     plt.title('Optim. method')
